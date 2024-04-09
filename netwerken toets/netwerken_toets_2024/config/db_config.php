cd1f91e49ab4dd5b93949b4c56a45344d6fedb4b
milan reijmer <milan.reijmer@student.graafschapcollege.nl>
2024-04-09 09:19:16 +0200

    goede inlog gegevens db

--- netwerken toets/netwerken_toets_2024/config/db_config.php
+++ netwerken toets/netwerken_toets_2024/config/db_config.php
@@ -2,7 +2,7 @@
 
 $dbhost = 'localhost:3306';
 $dbuser = 'net24mreijmer_milan';
-$dbpass = 'JanDeMan890';
+$dbpass = 'JanDeMan890!';
 $dbname = 'net24mreijmer_netwerkentoets';
 
 $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
